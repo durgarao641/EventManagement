167299f185c1f51d2208242390391e617dd1ab50
durga <durga.rao641@gmail.com>
2018-02-06 17:49:38 +0530

    updated

--- first/urls.py
+++ first/urls.py
@@ -3,5 +3,7 @@ from first import views
  
 urlpatterns = [
     url(r'^$', views.HomePageView.as_view()),
-    url(r'^login$', views.login, name='login'),
+    url(r'^events$', views.login, name='login'),
+    url(r'^dashboard$', views.dashboard, name='dashboard'),
+    url(r'^create_event$', views.create_event, name='create_event'),
 ]
--- first/views.py
+++ first/views.py
@@ -2,11 +2,6 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.http import HttpResponse
  
-events = [
-	{'name': 'ICC Cricket World cup', 'date': '25-feb-2018', 'organisors':'durga'},
-	{'name': 'FIFA', 'date': '10-mar-2018', 'organisors':'suresh'}
-]
-
 # Create your views here.
 class HomePageView(TemplateView):
     def get(self, request, **kwargs):
@@ -16,5 +11,31 @@ def login(request):
 	if request.method == "POST":
 		print(request.POST['name'])
 		print(request.POST['password'])
-		return render(request, 'dashboard.html', {'result': {'name': request.POST['name'], 'events': events}})
+		role = verify_user(request.POST['name'], request.POST['password'])
+		if role == 'admin':
+			return render(request, 'dashboard.html', {'result': {'name': request.POST['name'], 'events': get_events()}})
+		else:
+			return render(request, 'dashboard.html', {'result': {'name': request.POST['name'], 'events': get_events()}})
 	return render(request, 'index.html')
+
+def dashboard(request):
+	return render(request, 'dashboard.html', {'result': {'events': get_events()}})
+
+def create_event(request):
+	return render(request, 'create_event.html')
+
+def get_events():
+	events = [
+		{'name': 'ICC Cricket World cup', 'date': '25-feb-2018', 'organisors':'durga'},
+		{'name': 'FIFA', 'date': '10-mar-2018', 'organisors':'suresh'},
+		{'name': 'ICC Cricket World cup', 'date': '25-feb-2018', 'organisors':'durga'},
+		{'name': 'FIFA', 'date': '10-mar-2018', 'organisors':'suresh'}
+	]
+	return events
+
+def verify_user(username, password):
+	if username == 'durga':
+		return 'admin'
+	else:
+		return 'organizor'
+
