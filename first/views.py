4cba4cbeb7ad23d954b61d993fcce1cd6268d25f
durga <durga.rao641@gmail.com>
2018-02-06 14:33:18 +0530

    Added a dashboard

--- first/views.py
+++ first/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 from django.views.generic import TemplateView
 from django.http import HttpResponse
  
+events = [
+	{'name': 'ICC Cricket World cup', 'date': '25-feb-2018', 'organisors':'durga'},
+	{'name': 'FIFA', 'date': '10-mar-2018', 'organisors':'suresh'}
+]
+
 # Create your views here.
 class HomePageView(TemplateView):
     def get(self, request, **kwargs):
@@ -11,5 +16,5 @@ def login(request):
 	if request.method == "POST":
 		print(request.POST['name'])
 		print(request.POST['password'])
-		return render(request, 'login.html', {'result': request.POST['name']})
+		return render(request, 'dashboard.html', {'result': {'name': request.POST['name'], 'events': events}})
 	return render(request, 'index.html')
